3f31e2f5b1ada783134a15149431cb49249be181
Mark Wells <mark.s.wells@gmail.com>
2023-11-19 09:01:16 +0100

    Initial Commit

---
+++ FlickrImageGallery/FlickrImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrImageGalleryApp.swift
+//  FlickrImageGallery
+//
+//  Created by Mark Wells on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct FlickrImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
